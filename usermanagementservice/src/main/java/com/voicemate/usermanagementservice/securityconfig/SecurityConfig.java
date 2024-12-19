633128d764060c75038737cf719ddee91f085240
krishna <krishna@LAPTOP-KPRNONBC>
2024-12-19 23:00:26 +0530

    cmt

--- usermanagementservice/src/main/java/com/voicemate/usermanagementservice/securityconfig/SecurityConfig.java
+++ usermanagementservice/src/main/java/com/voicemate/usermanagementservice/securityconfig/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.voicemate.usermanagementservice.securityconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
@@ -17,8 +22,10 @@ public class SecurityConfig {
 																														// all
 																														// requests
 				).httpBasic(org.springframework.security.config.Customizer.withDefaults());
-		return http.build();
 
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		return http.build();
 	}
 }
 
@@ -27,4 +34,16 @@ public class SecurityConfig {
  * http.csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity (not
  * recommended for production) .authorizeHttpRequests(auth -> auth
  * .anyRequest().permitAll() // Permit all requests ); return http.build();
+ */
+
+/**
+ * Non lamda expression for http.csrf(csrf -> csrf.disable()) ;
+ */
+/*
+ * Customizer<CsrfConfigurer<HttpSecurity>> csCustomizer = new
+ * Customizer<CsrfConfigurer<HttpSecurity>>() {
+ * 
+ * @Override public void customize(CsrfConfigurer<HttpSecurity> customizer) {
+ * 
+ * customizer.disable(); } };
  */
